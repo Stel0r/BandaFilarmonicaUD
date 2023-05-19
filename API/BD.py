ca2a251166269220783c786cc93be3b61e02bbc5
Stel0r <65517739+Stel0r@users.noreply.github.com>
2023-05-18 19:52:26 -0500

    agregado endpoint para la agregacion del estudiante

--- API/BD.py
+++ API/BD.py
@@ -9,4 +9,15 @@ class ConexionBD:
         result = cursor.execute("SELECT * FROM ESTUDIANTE")
         for row in result:
             print(row)
+        return result
+
+    def agregarEstudiante(id,codigoProyecto,codigoDocumento,nombres,apellidos,fechaNacimiento,codigoEstudiante,numDocumento,correoPersonal,correoInstitucional,telefono):
+        connection = oracledb.connect(user="OrquestaUD", password="FilarmonicaUD",host="localhost", port=1521, service_name="xe")
+        cursor = connection.cursor()
+        query = "INSERT INTO estudiante values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        args = (id,codigoProyecto,codigoDocumento,nombres,apellidos,fechaNacimiento,codigoEstudiante,numDocumento,correoPersonal,correoInstitucional,telefono)
+        result = cursor.update(query,args)
+        connection.close()
+        return result
+
 
--- API/main.py
+++ API/main.py
@@ -4,6 +4,18 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from BD import ConexionBD
 
+class estudiante(BaseModel):
+    id:str
+    codigoProyecto:str
+    codigoDocumento:str
+    nombres:str
+    apellidos:str
+    fechaNacimiento:str
+    codigoEstudiante:str
+    numDocumento:str
+    correoPersonal:str
+    correoInstitucional:str
+    telefono:str
 
 app = FastAPI()
 
@@ -21,5 +33,10 @@ app.add_middleware(
 
 @app.get('/lista')
 async def root():
-    ConexionBD.consultarEstudiantes()
+    result = ConexionBD.consultarEstudiantes()
+    return {"message":"hola mi loco"}
+
+@app.post('/agregar')
+async def root(s:estudiante):
+    result = ConexionBD.agregarEstudiante(s.id, s.codigoProyecto, s.codigoDocumento, s.nombres, s.apellidos, s.fechaNacimiento, s.codigoEstudiante, s.numDocumento, s.correoPersonal, s.correoInstitucional, s.telefono)
     return {"message":"hola mi loco"}
