519a32ccb41a1334aee456d2c23d1da17d380a68
Stel0r <65517739+Stel0r@users.noreply.github.com>
2023-05-26 23:06:28 -0500

    queries primer punto calendario terminada

--- SQL/queriesCalendario.sql
+++ SQL/queriesCalendario.sql
@@ -24,18 +24,38 @@ Estructura:
 
 -- introducimos el Estado Activo: 
 
-insert into estado values('Activo')
+insert into estado values('Activo');
 
 
-insert into calendario values('MSIM','PA',0001,'Activo',to_date('02/06/2023 06:00 AM'. 'dd/mm/yyyy HH:MI AM'),to_date('07/06/2023 06:00 PM'. 'dd/mm/yyyy HH:MI AM'));
-insert into calendario values('MSIM','CN',0002,'Activo',to_date('08/06/2023 06:00 AM'. 'dd/mm/yyyy HH:MI AM'),to_date('10/06/2023 06:00 PM'. 'dd/mm/yyyy HH:MI AM'));
-insert into calendario values('MSIM','SL',0002,'Activo',to_date('11/06/2023 12:00 PM'. 'dd/mm/yyyy HH:MI AM'),to_date('11/06/2023 04:00 PM'. 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','PA',0001,'Activo',to_date('02/06/2023 06:00 AM', 'dd/mm/yyyy HH:MI AM'),to_date('07/06/2023 06:00 PM', 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','CN',0002,'Activo',to_date('08/06/2023 06:00 AM', 'dd/mm/yyyy HH:MI AM'),to_date('10/06/2023 06:00 PM', 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','SL',0003,'Activo',to_date('11/06/2023 12:00 PM', 'dd/mm/yyyy HH:MI AM'),to_date('11/06/2023 04:00 PM', 'dd/mm/yyyy HH:MI AM'));
 
 --ensayos
 
-insert into calendario values('MSIM','EN',0002,'Activo',to_date('12/06/2023 4:00 PM'. 'dd/mm/yyyy HH:MI AM'),to_date('12/06/2023 06:00 PM'. 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','EN',0004,'Activo',to_date('12/06/2023 4:00 PM', 'dd/mm/yyyy HH:MI AM'),to_date('12/06/2023 06:00 PM', 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','EN',0005,'Activo',to_date('15/06/2023 4:00 PM', 'dd/mm/yyyy HH:MI AM'),to_date('15/06/2023 06:00 PM', 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','EN',0006,'Activo',to_date('19/06/2023 4:00 PM', 'dd/mm/yyyy HH:MI AM'),to_date('19/06/2023 06:00 PM', 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','EN',0007,'Activo',to_date('22/06/2023 4:00 PM', 'dd/mm/yyyy HH:MI AM'),to_date('22/06/2023 06:00 PM', 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','EN',0008,'Activo',to_date('26/06/2023 4:00 PM', 'dd/mm/yyyy HH:MI AM'),to_date('26/06/2023 06:00 PM', 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','EN',0009,'Activo',to_date('29/06/2023 4:00 PM', 'dd/mm/yyyy HH:MI AM'),to_date('29/06/2023 06:00 PM', 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','EN',0010,'Activo',to_date('03/07/2023 4:00 PM', 'dd/mm/yyyy HH:MI AM'),to_date('03/07/2023 06:00 PM', 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','EN',0011,'Activo',to_date('06/07/2023 4:00 PM', 'dd/mm/yyyy HH:MI AM'),to_date('06/07/2023 06:00 PM', 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','EN',0012,'Activo',to_date('10/07/2023 4:00 PM', 'dd/mm/yyyy HH:MI AM'),to_date('10/07/2023 06:00 PM', 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','EN',0013,'Activo',to_date('13/07/2023 4:00 PM', 'dd/mm/yyyy HH:MI AM'),to_date('13/07/2023 06:00 PM', 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','EN',0014,'Activo',to_date('17/07/2023 4:00 PM', 'dd/mm/yyyy HH:MI AM'),to_date('17/07/2023 06:00 PM', 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','EN',0015,'Activo',to_date('20/07/2023 4:00 PM', 'dd/mm/yyyy HH:MI AM'),to_date('20/07/2023 06:00 PM', 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','EN',0016,'Activo',to_date('24/07/2023 4:00 PM', 'dd/mm/yyyy HH:MI AM'),to_date('24/07/2023 06:00 PM', 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','EN',0017,'Activo',to_date('27/07/2023 4:00 PM', 'dd/mm/yyyy HH:MI AM'),to_date('27/07/2023 06:00 PM', 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','EN',0018,'Activo',to_date('31/07/2023 4:00 PM', 'dd/mm/yyyy HH:MI AM'),to_date('31/07/2023 06:00 PM', 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','EN',0019,'Activo',to_date('03/08/2023 4:00 PM', 'dd/mm/yyyy HH:MI AM'),to_date('03/08/2023 06:00 PM', 'dd/mm/yyyy HH:MI AM'));
+
+--funciones
 
 
+insert into calendario values('MSIM','FN',0020,'Activo',to_date('04/08/2023 8:00 AM', 'dd/mm/yyyy HH:MI AM'),to_date('04/08/2023 10:00 AM', 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','FN',0021,'Activo',to_date('05/08/2023 8:00 AM', 'dd/mm/yyyy HH:MI AM'),to_date('05/08/2023 10:00 AM', 'dd/mm/yyyy HH:MI AM'));
+insert into calendario values('MSIM','FN',0022,'Activo',to_date('06/08/2023 8:00 AM', 'dd/mm/yyyy HH:MI AM'),to_date('06/08/2023 10:00 AM', 'dd/mm/yyyy HH:MI AM'));
 
 
 commit;
